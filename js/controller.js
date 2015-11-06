9a1cbe9850f82424f91fd9c96bca25dde90ec024
hanthom <m.hantho@gmail.com>
2015-11-06 17:09:03 -0600

    Updates made and HTML working

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,22 @@
-angular.module('giphy').controller('giphyCtrl', function($scope) {
+angular.module('giphy').controller('giphyCtrl', giphyCtrl);
+
+  function giphyCtrl($scope, giphyGotServed) {
 
 	$scope.test = "Hello world!"
 
-});
+	$scope.types = ['7/11s', 'restaurant', 'strip-clubs', 'zoos', 'bars', 'deli', 'barbers/salon', 'time-machines', 'prison'];
+	
+	$scope.addBiz = function() {
+	    giphyGotServed.postBiz($scope.biz);
+	    $scope.biz = '';
+	}
+	
+	$scope.getBiz = function() {
+	    giphyGotServed.getBiz().then(function(res) {
+	        $scope.biznesses = res.data.results;
+	    });
+	}
+
+	$scope.getBiz();
+
+};
